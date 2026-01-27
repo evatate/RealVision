cdbf393950fd296981c1fd54328b5cb9e8ed834c
Eva Tate <eva.n.tate.27@dartmouth.edu>
2026-01-27 10:47:15 -0800

    Added app icon and removed healthkit dependencies

--- app/lib/services/service_locator.dart
+++ app/lib/services/service_locator.dart
@@ -1,7 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'audio_service.dart';
 import 'camera_service.dart';
-import 'health_service.dart';
 import 'eye_tracking_service.dart';
 import 'motion_sensor_service.dart';
 import 'aws_auth_service.dart'; 
@@ -25,7 +24,6 @@ void setupServiceLocator() {
   /// App Services
   getIt.registerLazySingleton<AudioService>(() => AudioService());
   getIt.registerFactory<CameraService>(() => CameraService());
-  getIt.registerLazySingleton<HealthService>(() => HealthService());
   getIt.registerFactory<EyeTrackingService>(() => EyeTrackingService());
   getIt.registerFactory<MotionSensorService>(() => MotionSensorService());
 }
